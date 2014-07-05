1d75213ab0e9777419e3364b73b3d88dc57574bd
nogueirasjorge <nogueirasjorge@gmail.com>
2014-07-05 02:26:12 -0300

    Login
    Agregado de Archivo con PassWord y hash

--- Editor/Login.java
+++ Editor/Login.java
@@ -1,15 +1,20 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 
 public class Login extends JFrame {
@@ -17,8 +22,6 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField textField_1;
-	public static final String USER_NAME = "admin";
-	public static final String USER_PASS = "passW";
 	static Login _frame;
 
 	/**
@@ -73,31 +76,87 @@ public class Login extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				//Chequeo los parámetros de entrada
-				if(textField.getText().compareTo(USER_NAME) == 0 && textField_1.getText().compareTo(USER_PASS) == 0){
-					//Estoy logueando bien
-					new Editor();
-					_frame.setVisible(false);
-					
-				}else{
-					//Mensaje de error
-					textField.setText("ERROR EN EL LOGIN");
+				String miPass, user;
+				miPass = textField_1.getPassword().toString();
+				user = textField.getText();
+				
+				try {
+					if(validarDatos(miPass, user)){
+						new Editor();
+						_frame.setVisible(false);
+					}else{
+						JOptionPane.showMessageDialog(null, "Datos incorrectos. NO existe usuario y/o pass Incorrecto.");
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
 			}
 		});
 		btnIngresar.setBounds(317, 23, 89, 23);
 		contentPane.add(btnIngresar);
 		
-		JButton btnLimpiar = new JButton("Limpiar");
+		JButton btnLimpiar = new JButton("Nuevo");
 		btnLimpiar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				//Limpio los botones
-				textField.setText("");
-				textField_1.setText("");
+				if(textField.getText().length() != 0 && textField_1.getPassword().length != 0){
+					try {
+						nuevoUsuario(textField.getText(), textField_1.getPassword().toString());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}else{
+					JOptionPane.showMessageDialog(null, "Debe especificar Usuario y Pass");
+				}
 			
 			}
 		});
 		btnLimpiar.setBounds(317, 54, 89, 23);
 		contentPane.add(btnLimpiar);
+	}
+	
+	private void nuevoUsuario(String user, String pass) throws IOException{
+		//Estoy guardando un usuario nuevo
+		File passwd = new File(".//passwd");
+		String newUser;
+		newUser = user + ":" +  pass + "\n";
+		
+		FileWriter escribir = new FileWriter(passwd, true);
+		escribir.write(newUser);
+		
+		escribir.close();
+		
+	}
+	
+	private boolean validarDatos(String miPass, String user) throws IOException {
+		//Valido los datos
+		FileReader passwd = new FileReader("passwd");
+		BufferedReader lector = new BufferedReader(passwd);
+		String leido;
+		while((leido = lector.readLine()) != null){
+			String [] datos = leido.split(":");
+			
+			if(datos[0].compareTo(user) == 0){
+				//Encontré el usuario
+				passwd.close();
+				lector.close();
+				return verificarPassword(datos[1], miPass);
+			}
+		}
+		
+		passwd.close();
+		lector.close();
+		return false;
+		
+	}
+	
+	private boolean verificarPassword(String passS, String passE){
+		if(passE.length() > 16 || passE.length() < 4) return false; //Valores entre 4 y 8
+		if(passE.compareTo(passE) != 0) return false;
+		
+		return true;
 	}
 }
